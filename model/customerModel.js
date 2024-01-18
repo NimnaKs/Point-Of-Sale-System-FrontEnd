20069fef19ca486f6ced65d5bd36d8cf44b01a9b
NimnaKs <nimnakaveesha123456@gmail.com>
2024-01-18 10:28:02 +0530

    customer model done

---
+++ model/customerModel.js
@@ -0,0 +1,9 @@
+export class CustomerModel {
+    constructor(customerId, name, address, contact, email) {
+        this.customerId = customerId;
+        this.name = name;
+        this.address = address;
+        this.contact = contact;
+        this.email = email;
+    }
+}
